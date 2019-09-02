f44520362b103a6a3ddc248d3e7573b2795ce13a
井口 <outthere1983@gmail.com>
2019-09-02 15:47:11 +0000

    php/laravel 13 ニュース投稿画面を作成しよう　課題追加

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -7,25 +7,21 @@ use App\Http\Controllers\Controller;
 
 class ProfileController extends Controller
 {
-    //
-    public function add()
-    {
-        return view('admin.profile.create');
-    }
+  public function add()
+  {
+    return view('admin.profile.create');
+  }
     
-    public function create(Request $request)
-    {
-        
-        return redirect('admin/profile/create');
-    }
-    
-    public function edit()
-    {
-        return view('admin.profile.edit');
-    }
-    
-    public function update()
-    {
-        return redirect('admin/profile/edit');
-    }
+  public function create(Request $request)
+  {
+    return redirect('admin/profile/create');
+  }
+  public function edit()
+  {
+    return view('admin.profile.edit');
+  }
+   public function update(Request $request)
+  {
+    return redirect('admin/profile/edit');
+  }
 }
