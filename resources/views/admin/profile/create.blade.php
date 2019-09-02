cb23cf753e257e7f4ec23628fc3f254bc6c028fd
井口 <outthere1983@gmail.com>
2019-09-02 11:46:21 +0000

    php/laravel 12 課題再提出

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width", initial-scale=1>
+                    
+        <title>CreateProfile</title>
+    </head>
+    <body>
+        <h1>クリエイトプロフィール</h1>
+            @extends('layouts.profile')
+                    
+            @section('title', 'MY プロフィール')
+                    
+            @section('content')
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 mx-auto">
+                    <h2>MY プロフィール</h2>
+                </div>
+            </div>
+        </div>
+        @endsection
+    </body>
+</html>
