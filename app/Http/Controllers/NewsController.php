146aaad2ecd7bca94643aee6807fea52d5aa7d63
井口 <outthere1983@gmail.com>
2019-09-18 09:24:21 +0000

    php/laravel 18 課題　修正

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -11,16 +11,16 @@ class NewsController extends Controller
 {
     public function index(Request $request)
     {
-        $posts = News::all()->sortByDesc('update_at');
-        
+        $posts = News::all()->sortByDesc('updated_at');
+
         if (count($posts) > 0) {
             $headline = $posts->shift();
         } else {
             $headline = null;
         }
-        
-        //news/index.blade.php ファイルを渡している
-        //また　viewテンプレートにheadline,posts,という変数を渡している
+
+        // news/index.blade.php ファイルを渡している
+        // また View テンプレートに headline、 posts、という変数を渡している
         return view('news.index', ['headline' => $headline, 'posts' => $posts]);
     }
 }
